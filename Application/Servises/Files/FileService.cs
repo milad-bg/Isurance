74672297a339b13083b80837a87a872317db5316
mohammad bahger <mohammad.bagher.rezaee.1400@gmail.com>
2023-01-02 22:32:15 +0330

    fix file wwwroot

--- Application/Servises/Files/FileService.cs
+++ Application/Servises/Files/FileService.cs
@@ -24,47 +24,60 @@ namespace Application.Servises.Files
 
         public FileService(IFileRepository fileRepository,
             IUnitOfWork unitOfWork, ILogger logger, IMapper mapper)
-        {
+        {                                    
             _fileRepository = fileRepository;
             _unitOfWork = unitOfWork;
             _logger = logger;
             _mapper = mapper;
-        }
+        }                   
 
         public async Task<long?> UploadFileAsync(IFormFile file, FileUploadCommand command)
         {
-            var fileName = NameGenerator(file.FileName, out var format);
-            var filePath = Path.Combine("httpdocs\\wwwroot", fileName);
-            var size = file.Length;
-
-            if (format != "png" && format != "jpg" && format != "mp4")
-                throw new FormatException("فرمت های معتبر:png/jpg/mp4 خطا\n!");
-
-            switch (format)
+            try
             {
-                case "png":
-                case "jpg" when size > 15000000:
-                    throw new InvalidDataException(" حجم عکس شماباید کمتر از15مگابیت باشد!");
-                case "mp4" when size > 500000000:
-                    throw new InvalidDataException(" حجم فیلم شماباید کمتر از500 مگابایت باشد!");
+                var fileName = NameGenerator(file.FileName, out var format);
+
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", fileName);
+
+                var size = file.Length;
+
+                if (format != "png" && format != "jpg" && format != "mp4")
+                    throw new FormatException("فرمت های معتبر:png/jpg/mp4 خطا\n!");
+
+                switch (format)
+                {
+                    case "png":
+                    case "jpg" when size > 15000000:
+                        throw new InvalidDataException(" حجم عکس شماباید کمتر از15مگابیت باشد!");
+                    case "mp4" when size > 500000000:
+                        throw new InvalidDataException(" حجم فیلم شماباید کمتر از500 مگابایت باشد!");
+                }
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                var newFile = new Domain.Domain.Entities.File.File();
+
+                newFile.Url = fileName;
+                newFile.Size = size;
+                newFile.FilePath = filePath;
+                newFile.CreationDate = DateTime.Now;
+                newFile.MediaEntity = command.MediaType;
+
+                await _fileRepository.UploadFileAsync(newFile);
+                await _fileRepository.SaveChanges();
+                return newFile.Id;
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception, "{Repo} Add method error", typeof(FileService));
+
+                throw new Exception("erro catch");
             }
 
-            using (var stream = new FileStream(filePath, FileMode.Create))
-            {
-                await file.CopyToAsync(stream);
-            }
 
-            var newFile = new Domain.Domain.Entities.File.File();
-
-            newFile.Url = fileName;
-            newFile.Size = size;
-            newFile.FilePath = filePath;
-            newFile.CreationDate = DateTime.Now;
-            newFile.MediaEntity = command.MediaType;
-
-            await _fileRepository.UploadFileAsync(newFile);
-            await _fileRepository.SaveChanges();
-            return newFile.Id;
         }
 
         public async Task<FileDto> DownloadFile(long id)
